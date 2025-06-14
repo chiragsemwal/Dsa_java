44195abe2054aa209d0136a48ec49ebf58c19287
Chirag Sharma <chiragsemwal7@gmail.com>
2025-06-14 15:43:33 +0530

    Initial commit

---
+++ src/Arrays/ArrayBasics.java
@@ -0,0 +1,12 @@
+package Arrays;
+
+public class ArrayBasics {
+    /*
+    Syntax ->
+            data_type[] varible_name = new data_type[size];
+     */
+    public static void main(String[] args) {
+        int[] arr = new int[5];
+        arr[1] = 5;
+    }
+}
---
+++ src/Arrays/SwapArray.java
@@ -0,0 +1,13 @@
+package Arrays;
+
+import java.util.Arrays;
+
+public class SwapArray {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5};
+        swap(arr, 1,4);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void swap(int[] arr, int index1, int )
+}
