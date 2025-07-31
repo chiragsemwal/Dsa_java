82898f3c7b7982ad71631071d86a186e5e90be29
Chirag Sharma <chiragsemwal7@gmail.com>
2025-07-31 21:21:47 +0530

    finding smallest letter greater than target solved

---
+++ src/Arrays/BinarySearch/FirstAndLastPositionOfElement.java
@@ -0,0 +1,12 @@
+package Arrays.BinarySearch;
+
+public class FirstAndLastPositionOfElement {
+    public static void main(String[] args) {
+
+    }
+
+    static int search(int[] arr, int target){
+        // TODO: 01/08/25
+        return 1;
+    }
+}
